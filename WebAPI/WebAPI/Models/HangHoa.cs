540f95da09939dbe9af2c290e64ede32492b57ee
LegendStack12 <95231412+LegendStack12@users.noreply.github.com>
2022-07-29 17:34:08 +0700

    Add files via upload

---
+++ WebAPI/WebAPI/Models/HangHoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace WebAPI.Models
+{
+    public class HangHoaLego
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+
+    public class HangHoa : HangHoaLego
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+
+}
